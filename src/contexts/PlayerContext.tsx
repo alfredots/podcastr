1e022257f4ed011095bd6cadd72efbe06114b05b
alfredots <tito.alfred@hotmail.com>
2021-04-24 17:11:04 -0300

    projeto finalizado

---
+++ src/contexts/PlayerContext.tsx
@@ -0,0 +1,121 @@
+import {createContext, ReactNode, useContext, useState} from "react";
+
+type Episode = {
+  title: string;
+  members: string;
+  thumbnail: string;
+  duration: number;
+  url: string
+}
+
+type PlayerContextData = {
+  episodeList: Episode[];
+  currentEpisodeIndex: number;
+  isPlaying: boolean;
+  isLooping: boolean;
+  isShuffling: boolean;
+  hasNext: boolean;
+  hasPrevious: boolean;
+  play:(episode: Episode) => void;
+  playList:(episodes: Episode[], index: number) => void;
+  setPlayingState: (state:boolean) => void;
+  clearPlayingState: () => void;
+  tooglePlay:() => void;
+  toogleLoop:() => void;
+  toogleShuffle:() => void;
+  playNext:() => void;
+  playPrevious:() => void;
+}
+
+export const PlayerContext = createContext({} as PlayerContextData )
+
+type PlayerContextProviderProps = {
+  children: ReactNode;
+}
+
+export  function PlayerContextProvider ({ children }: PlayerContextProviderProps) {
+  const [episodeList, setEpisodeList] = useState([])
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [isLooping, setIsLooping] = useState(false)
+  const [isShuffling, setIsShuffling] = useState(false)
+
+  function play(episode: Episode) {
+    setEpisodeList([episode])
+    setCurrentEpisodeIndex(0)
+    setIsPlaying(true)
+  }
+
+  function playList(list: Episode[], index: number) {
+    setEpisodeList(list)
+    setCurrentEpisodeIndex(index)
+    setIsPlaying(true)
+  }
+
+  function tooglePlay() {
+    setIsPlaying(!isPlaying)
+  }
+
+  function toogleLoop() {
+    setIsLooping(!isLooping)
+  }
+
+  function toogleShuffle() {
+    setIsShuffling(!isShuffling)
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state)
+  }
+
+  function clearPlayingState() {
+    setEpisodeList([])
+    setCurrentEpisodeIndex(0)
+  }
+
+  const hasNext = isShuffling || (currentEpisodeIndex + 1) < episodeList.length
+
+  function playNext() {
+    if (isShuffling) {
+      const nextRandomEpisodeIndex = Math.floor(Math.random() *episodeList.length)
+      setCurrentEpisodeIndex(nextRandomEpisodeIndex)
+    } else  if (hasNext) {
+      setCurrentEpisodeIndex(currentEpisodeIndex + 1)
+    }
+
+  }
+
+  const hasPrevious = currentEpisodeIndex > 0
+
+  function playPrevious() {
+    if (hasPrevious) {
+      setCurrentEpisodeIndex(currentEpisodeIndex - 1)
+    }
+  }
+
+  return (
+    <PlayerContext.Provider value={{
+      episodeList,
+      currentEpisodeIndex,
+      play,
+      playList,
+      isPlaying,
+      isLooping,
+      isShuffling,
+      tooglePlay,
+      toogleLoop,
+      toogleShuffle,
+      setPlayingState,
+      clearPlayingState,
+      playNext,
+      playPrevious,
+      hasNext,
+      hasPrevious }}>
+      { children }
+    </PlayerContext.Provider>
+  )
+}
+
+export const usePlayer = () => {
+  return useContext(PlayerContext)
+}
---
+++ src/pages/episode/[slug].tsx
@@ -0,0 +1,110 @@
+import {useRouter} from "next/router";
+import {GetStaticPaths, GetStaticProps} from 'next'
+import {api} from "../../services/api";
+import Image from 'next/image'
+import Link from 'next/link'
+import {format, parseISO} from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import {convertDurationToTimeString} from "../../utils/convertDurationToTimeString";
+
+
+import styles from './episode.module.scss'
+import {usePlayer} from "../../contexts/PlayerContext";
+import Head from "next/head";
+
+type Episode = {
+  id: string,
+  title: string,
+  thumbnail: string,
+  members: string,
+  publishedAt: string,
+  duration: number,
+  durationAsString: string,
+  description: string,
+  url: string
+}
+
+type EpisodeProps = {
+  episode: Episode
+}
+
+export default function Episode({ episode }: EpisodeProps) {
+  const {play} = usePlayer()
+  const router = useRouter()
+  return (
+    <div className={styles.episodeContainer}>
+      <Head>
+        <title>{episode.title} | Podcastr</title>
+      </Head>
+      <div className={styles.thumbnailContainer}>
+        <Link href="/">
+          <button type="button">
+            <img src="/arrow-left.svg" alt="voltar"/>
+          </button>
+        </Link>
+        <Image width={700} height={168} src={episode.thumbnail} objectFit="fill" />
+        <button type="button" onClick={() => play(episode)}>
+          <img src="/play.svg" alt="Tocar episódio" />
+        </button>
+      </div>
+
+      <header>
+        <h1>{episode.title}</h1>
+        <span>{episode.members}</span>
+        <span>{episode.publishedAt}</span>
+        <span>{episode.durationAsString}</span>
+      </header>
+
+      <div className={styles.description}
+           dangerouslySetInnerHTML={{__html: episode.description}} />
+    </div>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const paths = data.map(episode => {
+    return {
+      params: {
+        slug: episode.id
+      }
+    }
+  })
+
+  return {
+    paths,
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { slug } = ctx.params;
+
+  const { data } = await api.get(`/episodes/${slug}`)
+
+  const episode = {
+    id: data.id,
+    title: data.title,
+    thumbnail: data.thumbnail,
+    members: data.members,
+    publishedAt: format(parseISO(data.published_at), 'd MMM yy', { locale: ptBR}),
+    duration: Number(data.file.duration),
+    durationAsString: convertDurationToTimeString(Number(data.file.duration)),
+    description: data.description,
+    url: data.file.url
+  }
+
+  return {
+    props: {
+      episode
+    },
+    revalidate: 60 * 60 * 24
+  }
+}
